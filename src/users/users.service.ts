e579798c582e83bc15c084edd73b274f47df75a4
Baltasar Romero <baromeg@gmail.com>
2025-03-22 16:27:36 +0000

    improved logging on auth methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import * as bcrypt from 'bcryptjs';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
+  private readonly logger = new Logger(UsersService.name);
   constructor(private prisma: PrismaService) {}
 
-  async createUser(data: { email: string; password: string; firstName: string; lastName: string }) {
+  async createUser(data: {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+  }): Promise<User> {
+    this.logger.log(`Creating user with email: ${data.email}`);
+
     const hashedPassword = await bcrypt.hash(data.password, 10);
-    return this.prisma.user.create({
+    const user = await this.prisma.user.create({
       data: {
         email: data.email,
         firstName: data.firstName,
@@ -16,13 +25,21 @@ export class UsersService {
         passwordHash: hashedPassword,
       },
     });
+
+    this.logger.log(`User created successfully: ${user.email}`);
+
+    return user;
   }
 
   async updateRefreshToken(userId: string, hashedToken: string) {
-    return this.prisma.user.update({
+    const token = this.prisma.user.update({
       where: { id: userId },
       data: { hashedRefreshToken: hashedToken },
     });
+
+    this.logger.log(`Updated refresh token for userId=${userId}`);
+
+    return token;
   }
 
   async findByEmail(email: string) {
